5a36cb87d397da8700f42d0d9b835e42335ab648
megatronus@sina.cn <megatronus@sina.cn>
2018-09-08 21:39:11 +0800

    0

--- app/src/main/java/com/megatronus/weather/WeatherActivity.java
+++ app/src/main/java/com/megatronus/weather/WeatherActivity.java
@@ -7,22 +7,40 @@ import com.megatronus.weather.bean.Forecast;
 
 public class WeatherActivity extends AppCompatActivity
 {
+	private TextView temperature ;
 	private TextView weather ;
-
+	private TextView wind ;
+	private TextView hum ;
+	
+	private TextView msg ;
+	private TextView update ;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_weather);
-		weather = (TextView) findViewById(R.id.weather_temperature);
-		
+		weather = (TextView) findViewById(R.id.weather_weather);
+		temperature = (TextView) findViewById(R.id.weather_temperature);
+		wind = (TextView) findViewById(R.id.weather_wind);
+		hum = (TextView) findViewById(R.id.weather_hum);
+		msg = (TextView) findViewById(R.id.weather_msg);
+		update = (TextView) findViewById(R.id.weather_update);
 		
 		new Weather<Forecast>(Forecast.class, new  Weather.CallBack<Forecast>(){
 
 				@Override
 				public void result(Forecast json)
 				{
-					weather.setText(json.getHeWeather6().daily_forecast.get(0).tmp_max + "°");
+					
+					Forecast.HeWeather.Daily_Forecast Df = json.getHeWeather6().daily_forecast.get(0);
+					temperature.setText(Df.tmp_max + "°");
+					weather.setText(Df.cond_txt_d);
+					wind.setText(Df.wind_dir + " 风力 " +Df.wind_sc);
+					hum.setText("相对湿度  " + Df.hum);
+					msg.setText("降水概率   :" +Df.pop + "\n最低温度   :"+Df.tmp_min + "\n预报日期   :" + Df.date );
+					
+					update.setText("更新时间\n"+json.getHeWeather6().update.loc);
 				}
 			}).execute(Conn.FIRECAST);
 	}
